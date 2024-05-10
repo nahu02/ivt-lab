ce7b9f13572c6801bd32deec4342b2c52c70388d
CPD63P <nagy.abel@edu.bme.hu>
2024-05-10 08:59:14 +0200

    fix both fire

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -80,7 +80,9 @@ public class GT4500 implements SpaceShip {
       case ALL:
         // try to fire both of the torpedo stores
         if (! primaryTorpedoStore.isEmpty() && ! secondaryTorpedoStore.isEmpty()) {
-          firingSuccess = primaryTorpedoStore.fire(1) || secondaryTorpedoStore.fire(1);
+          boolean firstFired = primaryTorpedoStore.fire(1);
+          boolean secondaryFired = secondaryTorpedoStore.fire(1);
+          firingSuccess = firstFired || secondaryFired;
 	  wasPrimaryFiredLast = false;
 	}
         else {
